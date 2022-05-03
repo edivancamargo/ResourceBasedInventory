9d9dd70b30f1f75c20e24c421c7c79fca6ac2e5d
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-03 23:17:39 +0100

    Inventory Slot drag'n drop system working.

---
+++ inventory/InventorySlot.gd
@@ -0,0 +1,33 @@
+extends CenterContainer
+
+onready var itemTextureRect = $ItemTextureRect
+var emptyInventorySlot = preload("res://assets/items/EmptyInventorySlot.png")
+var inventory = preload("res://inventory/Inventory.tres")
+
+func display_item(item: Item):
+	if item is Item:
+		itemTextureRect.texture = item.texture
+	else:
+		itemTextureRect.texture = emptyInventorySlot
+
+func get_drag_data(position):
+	var item_idx = get_index()
+	var item = inventory.remove_item(item_idx)
+	if item is Item:
+		var data = {}
+		data.item = item
+		data.item_idx = item_idx
+		
+		var dragPreview = TextureRect.new()
+		dragPreview.texture = item.texture
+		set_drag_preview(dragPreview)
+		return data
+	
+func can_drop_data(position, data):
+	return data is Dictionary and data.has("item")
+
+func drop_data(position, data):
+	var my_item_idx = get_index()
+	var my_item = inventory.items[my_item_idx]
+	inventory.swap_items(my_item_idx, data.item_idx)
+	inventory.set_item(my_item_idx, data.item)
