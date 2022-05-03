2feab525aac33a43e5b5f727da44a073a9d20ca3
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-03 23:40:12 +0100

    Impor font, create dynamic font resource and delete InventoryContainer script from project.

--- inventory/InventoryContainer.gd
+++
@@ -1,9 +0,0 @@
-extends ColorRect
-
-const inventory = preload("res://inventory/Inventory.tres")
-
-func can_drop_data(position, data):
-	return data is Dictionary and data.has("item")
-
-func drop_data(position, data):
-	inventory.set_item(data.item_idx, data.item)
